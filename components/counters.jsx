a59b45f33cdd0d23e2eba0bd411feeb7cbea390f
KrisHarrison <33849103+KrisHarrison@users.noreply.github.com>
2020-03-01 17:37:03 -0500

    Add files via upload

--- components/counters.jsx
+++ components/counters.jsx
@@ -5,9 +5,12 @@ import '../css/counters.css'
 class Counters extends Component {
 
     render() { 
-        const {counters, onIncrement, onDecrement, onDelete} = this.props;
+        const {counters, onIncrement, onDecrement, onDelete, onReset} = this.props;
         return (
             <div className="counters">
+              <button className="reset" onClick={onReset}>
+                Reset
+              </button>
                 {counters.map(counter =>
                   <Counter
                     key={counter.id}
